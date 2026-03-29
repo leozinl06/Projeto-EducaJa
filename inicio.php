47cdeab90c945315ba57d7dc0b9348fae7ed96e5
Dominic Skiba <skibadominic@gmail.com>
2026-03-29 15:27:23 -0300

    feat: Pagina inicial criada, arquivos de login e cadastro adicionados

---
+++ inicio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Início - EducaJá</title>
+    <link rel="stylesheet" href="assets/css/inicio.css">
+</head>
+<body>
+    <main class="container-incio">
+        <div class ="card-boas-vindas">
+            <h1 class="logo-educaja">EducaJá</h1>
+
+            <p class="mensagem-boas-vindas">Bem-Vindo(a)! Escolha uma opção</p>
+
+            <div class="grupos-botoes">
+                <a href="login.php" class="btn btn-entrar">Entrar</a>
+                <a href="cadastro.php" class="btn btn-criar">Criar Conta</a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
